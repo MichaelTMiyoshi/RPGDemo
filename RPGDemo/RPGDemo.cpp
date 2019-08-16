73d724357b1f32104f8df0c1167afe2118a57ff1
Michael T. Miyoshi <MichaelTMiyoshi@gmail.com>
2019-08-16 15:30:29 -0700

    Added equip loop and game loop. Game loop ends when you have a pencil in room 2.

--- RPGDemo/RPGDemo.cpp
+++ RPGDemo/RPGDemo.cpp
@@ -3,10 +3,20 @@ Author:		Miyoshi
 Project:	RPG Demo
 Purpose:	Simple console RPG and GitHub Repo
 Pseudocode:
+	-----GS1-4
 	Output intro to the screen
+	-----GS3-1
 	Create variables for health, hit points, and name
 	Choose max and min for health and hit points and randomly give them to the user
 	Address the user by name where appropriate
+	-----GS4-6
+	main menu in a loop
+		equip user
+			equip user with at least one item
+			must have 3 choices for item
+		enter game
+	game loop
+
 
 Maintenance Log:
 08/16/2019	Began program from scratch
@@ -19,6 +29,18 @@ Maintenance Log:
 			https://blogs.msdn.microsoft.com/benjaminperkins/2016/04/01/setting-up-and-using-github-in-visual-studio-2015/
 			13:14	create variables and give them values (GS3-1)
 			13:30	coded and tested.  Update to repository as branch variables.
+					Need to make sure to PUBLISH the branch after updating.
+			13:38	game loop and branching (GS4-6)
+					made a branch off of variables.
+			15:25	created the equip loop.
+					created the game loop.
+						three rooms.  (numbered 0, 1, 2)
+						in room 1 is a pencil.  If you do not have a pencil, you can trade.
+							if you decide not to trade, you cannot win
+							when you come back again, you get second chance to exchange for the pencil
+						room 2 is the exit.  if you have the pencil.  if not, you go back to room 0
+					added an equipment name
+					added a way to say you really want to exchange for the pencil
 */
 
 #include <stdio.h>
@@ -43,6 +65,7 @@ int main()
 	printf("Let us start by you telling me your name >>>  ");
 	string name;
 	getline(cin, name, '\n');	// let the user have spaces in name
+	fseek(stdin, 0, SEEK_END);
 	int min = 5, max = 10;
 	int health;
 	int hitPoints;
@@ -51,6 +74,142 @@ int main()
 	hitPoints = min + rand() % (max - min + 1);
 
 	printf("Well hello %s.\nYour stats are:\n\tHealth:\t\t%d\n\tHit Points:\t%d\n", name.c_str(), health, hitPoints);
+
+//GS4-6 main menu and game loop
+	bool ready = false;
+	int equipment = 0;
+	string equipmentName = "";
+	do 
+	{
+		char choice;
+		printf("\n\nYou get to choose one item before you begin.\n");
+		printf("Which would you like?\n");
+		printf("\tA. Pencil\n");
+		printf("\tB. Laptop computer\n");
+		printf("\tC. Book of matches\n");
+		printf(">>>  ");
+		scanf("%c", &choice);
+		fseek(stdin, 0, SEEK_END);
+		ready = true;
+		switch (choice)
+		{
+		case 'A': case 'a':
+			equipment = 1;
+			equipmentName = "pencil";
+			break;
+		case 'B': case 'b':
+			equipment = 2;
+			equipmentName = "laptop computer";
+			break;
+		case 'C': case 'c':
+			equipment = 3;
+			equipmentName = "book of matches";
+			break;
+		default:
+			printf("You must choose to arm yourself with something.");
+			ready = false;
+			break;
+		}
+
+		if (equipment != 0)
+		{
+			printf("A %s is a great choice.", equipmentName.c_str());
+		}
+
+		if (!ready)
+		{
+			continue;
+		}
+
+		printf("\n\n\nAre you ready to play the game?  >>>  ");
+		scanf("%c", &choice);
+		fseek(stdin, 0, SEEK_END);
+		if (toupper(choice) == 'N')
+		{
+			ready = false;
+			printf("\nOkay.  Let us equip you again.\n");
+		}
+	} while (!ready);
+
+	bool exit = false;
+	int room = 0;
+	int timesThrough = 0;
+
+	do
+	{
+		char choice;
+		if (room == 0)
+		{
+			printf("This is the first room\n");
+			printf("Would you like to go to the second room?  (Y/N)  >>>  ");
+			scanf("%c", &choice);
+			fseek(stdin, 0, SEEK_END);
+			choice = toupper(choice);
+			if (choice == 'Y')
+			{
+				room = 1;
+			}
+		}
+		else if (room == 1)
+		{
+			printf("This is the second room\n");
+			if (equipment != 1)	// not a pencil
+			{
+				printf("You found a pencil.\n");
+				printf("Would you like to trade your %s for a pencil?  (Y/N)  >>>  ", equipmentName.c_str());
+				scanf("%c", &choice);
+				fseek(stdin, 0, SEEK_END);
+				choice = toupper(choice);
+				if (choice == 'Y')
+				{
+					equipment = 1;
+				}
+				else
+				{
+					if (1 <= timesThrough)
+					{
+						printf("Are you sure you do not want the pencil?  (Y/N)  >>>  ");
+						scanf("%c", &choice);
+						fseek(stdin, 0, SEEK_END);
+						choice = toupper(choice);
+						if (choice == 'N')
+						{
+							continue;	// go back to the top of the loop
+						}
+
+					}
+				}
+			}
+
+			printf("\nWould you like to go to the third room?  (Y/N)  >>>  ");
+			scanf("%c", &choice);
+			fseek(stdin, 0, SEEK_END);
+			choice = toupper(choice);
+			if (choice == 'Y')
+			{
+				room = 2;
+			}
+			timesThrough++;
+		}
+		else if (room == 2)
+		{
+			printf("This is the third room\n");
+			if (equipment != 1)
+			{
+				printf("\n\nYou do not have a pencil.  You  must go back to the first room.\n");
+				room = 0;
+				printf("Press a key to go to the first room.\n\n");
+				_getch();
+				fseek(stdin, 0, SEEK_END);
+			}
+			else
+			{
+				exit = true;
+			}
+		}
+	} while (!exit);
+
+	printf("\n\nWay to go!  You made it past all the competency tests!\n\n");
 
 	// keep these all together at the end of main()
 	printf("\n\nPress the any key to exit...");
